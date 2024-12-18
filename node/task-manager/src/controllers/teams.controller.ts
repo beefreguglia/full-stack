88e24351fc1e0efe51da2b12591297dc4206afdd
Bernardo <beefreguglia@gmail.com>
2024-12-17 22:22:25 -0300

    feature: create teams controller and routes

---
+++ node/task-manager/src/controllers/teams.controller.ts
@@ -0,0 +1,25 @@
+import { prisma } from '@/database/prisma';
+import { Request, Response } from 'express'
+import { z } from 'zod'
+
+class TeamsController {
+  async create(request: Request, response: Response) {
+    const teamBodySchema = z.object({
+      name: z.string(),
+      description: z.string().optional(),
+    });
+
+    const { name, description } = teamBodySchema.parse(request.body);
+
+    const team = await prisma.team.create({
+      data: {
+        name,
+        description,
+      }
+    });
+
+    return response.status(201).json({ team });
+  }
+}
+
+export { TeamsController }
---
+++ node/task-manager/src/routes/teams.routes.ts
@@ -0,0 +1,10 @@
+
+import { TeamsController } from "@/controllers/teams.controller";
+import { Router } from "express";
+
+const teamsRoutes = Router()
+const teamsController = new TeamsController()
+
+teamsRoutes.post("/", teamsController.create)
+
+export { teamsRoutes }
