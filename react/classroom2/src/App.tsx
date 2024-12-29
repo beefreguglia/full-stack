e0501c389f9cd82cfcc6da75199e329d706bbeb7
Bernardo <beefreguglia@gmail.com>
2024-12-29 19:09:49 -0300

    feature: create tip components

--- react/classroom2/src/App.tsx
+++ react/classroom2/src/App.tsx
@@ -1,16 +1,18 @@
 import styles from './app.module.css'
 
 import { Header } from "./components/Header"
+import { Tip } from './components/Tip'
 
 function App() {
   function handleRestartGame() {
     alert("Reiniciar jogo")
   }
-  
+
   return (
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Tip tip='Uma das linguagens de programação mais utilizada' />
       </main>
     </div>
   )
