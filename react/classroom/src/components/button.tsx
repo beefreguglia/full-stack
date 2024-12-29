8a5bfebdfd50ef760626dc932757d46492f63f3e
Bernardo <beefreguglia@gmail.com>
2024-12-29 11:14:01 -0300

    feature: create react first classroom

--- react/classroom/src/components/button.tsx
+++ react/classroom/src/components/button.tsx
@@ -4,6 +4,6 @@ type ButtonProps = ComponentProps<"button"> & {
   name: string;
 }
 
-export function Button({ name, rest }: ButtonProps) {
+export function Button({ name, ...rest }: ButtonProps) {
   return <button {...rest}>{name}</button>
 }
