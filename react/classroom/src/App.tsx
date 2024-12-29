c806ea1c34c947bbd5ce6c0b46edbfc26dcfd72c
Bernardo <beefreguglia@gmail.com>
2024-12-29 16:30:01 -0300

    feature: create first hook and use useState

--- react/classroom/src/App.tsx
+++ react/classroom/src/App.tsx
@@ -1,14 +1,29 @@
 import "./global.css";
 
+import { useState } from 'react';
 import { Button } from "./components/button";
+
 import styles from "./app.module.css";
 
+// import { useMessage } from "./hooks/useMessage";
+
 export function App() {
+  // const { show } = useMessage("Bernardo", 28);
+  const [count, setCount] = useState(0)
+
+  function handleAdd() {
+    setCount(count + 1)
+  }
+
+  function handleRemove() {
+    setCount(count - 1)
+  }
+
   return (
     <div className={styles.container}>
-      <Button name="Adicionar" />
-      <span>0</span>
-      <Button name="Remover" />
+      <Button name="Adicionar" onClick={handleAdd} />
+      <span>{count}</span>
+      <Button name="Remover" onClick={handleRemove} />
     </div>
   )
 }
