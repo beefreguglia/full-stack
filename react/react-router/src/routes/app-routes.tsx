bb2ccedee0b24d26c4da438a0d47a6fcabb410d0
Bernardo <beefreguglia@gmail.com>
2025-01-10 15:21:08 -0300

    feature: using layout

--- react/react-router/src/routes/app-routes.tsx
+++ react/react-router/src/routes/app-routes.tsx
@@ -4,13 +4,16 @@ import { Home } from '../pages/home'
 import { Products } from '../pages/products'
 import { NotFound } from '../pages/not-found'
 import { Details } from '../pages/details'
+import { Layout } from '../components/layout'
 
 export function AppRoutes() {
   return(
     <Routes>
-      <Route path='/' index element={<Home />}  />
-      <Route path='/products' element={<Products />}  />
-      <Route path='/details/:id' element={<Details />}  />
+      <Route path='/' element={<Layout />}>
+        <Route path='/' index element={<Home />}  />
+        <Route path='/products' element={<Products />}  />
+        <Route path='/details/:id' element={<Details />}  />
+      </Route>
 
       <Route path='*' element={<NotFound />}  />
     </Routes>
