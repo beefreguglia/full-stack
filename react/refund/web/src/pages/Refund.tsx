fb626b909cd6e0219d6ab807663c2b0e0efb92d4
Bernardo <beefreguglia@gmail.com>
2025-02-11 13:11:13 -0300

    feature: create states for inputs

--- react/refund/web/src/pages/Refund.tsx
+++ react/refund/web/src/pages/Refund.tsx
@@ -3,12 +3,25 @@ import { Input } from "../components/Input";
 import { Select } from "../components/select";
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Upload } from "../components/Upload";
+import { Button } from "../components/Button";
 
 export function Refund() {
+  const [name, setName] = useState("")
+  const [amount, setAmount] = useState("")
   const [category, setCategory] = useState("")
+  const [fileName, setFilename] = useState<File | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    console.log(name, category, amount, fileName);
+  }
 
   return (
-    <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+    <form 
+      onSubmit={onSubmit} 
+      className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]"
+    >
       <header>
         <h1 className="text-xl font-bold text-gray-100">Solicitação de reembolso</h1>
         <p className="text-sm text-gray-200 mt-2 mb-4">Dados da despesa para solicitar reembolso.</p>
@@ -16,6 +29,8 @@ export function Refund() {
       <Input 
         required
         legend="Nome da solicitação"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
       <div className="flex gap-4">
         <Select 
@@ -36,9 +51,15 @@ export function Refund() {
         <Input 
           required
           legend="Valor"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
         />
       </div>
-      <Upload />
+      <Upload
+        filename={fileName?.name}
+        onChange={(e) => e.target.files && setFilename(e.target.files[0])}
+      />
+      <Button type="submit" isLoading={isLoading}>Enviar</Button>
     </form>
   )
 }
