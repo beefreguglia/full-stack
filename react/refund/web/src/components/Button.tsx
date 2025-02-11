45b3c52126513e47315c55c13472e5e782f013b3
Bernardo <beefreguglia@gmail.com>
2025-02-11 14:28:14 -0300

    feature: create button icon variant

--- react/refund/web/src/components/Button.tsx
+++ react/refund/web/src/components/Button.tsx
@@ -1,16 +1,31 @@
+import { classMerge } from "../utils/classMerge";
+
 type Props = React.ComponentProps<"button"> & {
   isLoading?: boolean;
+  variant?: "base" | "icon";
 }
 
+const variants = {
+  button: {
+    base: "h-12",
+    icon: "h-12 w-12",
+  },
+}
 export function Button({ 
   isLoading, 
   children, 
-  type = "button", 
+  type = "button",
+  variant = "base",
   ...rest 
 }: Props) {
   return (
     <button 
-      className="flex items-center justify-center bg-green-100 text-white cursor-pointer hover:bg-green-200 transition ease-linear rounded-lg disabled:opacity-50 disabled:cursor-not-allowed h-12"
+      className={
+        classMerge([
+          "flex items-center justify-center bg-green-100 text-white cursor-pointer hover:bg-green-200 transition ease-linear rounded-lg disabled:opacity-50 disabled:cursor-not-allowed",
+          variants.button[variant],
+        ])
+      }
       type={type} 
       {...rest}
     >
--- react/refund/web/src/pages/Dashboard.tsx
+++ react/refund/web/src/pages/Dashboard.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+import searchSvg from "../assets/search.svg"
 export function Dashboard() {
+  const [name, setName] = useState("")
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    console.log(name)
+  }
+
   return(
     <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px]">
       <h1 className="text-gray-100 font-bold text-xl flex-1">
         Solicitações
       </h1>
+      <form
+        onSubmit={onSubmit}
+        className="flex items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md: flex-row gap-2 mt-6"
+      >
+        <Input 
+          placeholder="Pesquisar pelo nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <Button type="submit" variant="icon">
+          <img src={searchSvg} alt="Ícone de pesquisar" className="w-5" />
+        </Button>
+      </form>
     </div>
   )
 }
