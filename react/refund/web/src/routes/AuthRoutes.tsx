7cff34b19287dcd6a331795ef316eefe950959c7
Bernardo <beefreguglia@gmail.com>
2025-02-11 12:16:05 -0300

    feature: not found page

--- react/refund/web/src/routes/AuthRoutes.tsx
+++ react/refund/web/src/routes/AuthRoutes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import { SignIn } from "../pages/SignIn";
 import { AuthLayout } from "../components/AuthLayout";
 import { SignUp } from "../pages/SignUp";
+import { NotFound } from "../pages/NotFound";
 
 export function AuthRoutes() {
 
@@ -11,6 +12,8 @@ export function AuthRoutes() {
         <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Route>
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
