b5217b68cd9ac8b17a85034dccfc3b7a70c3b8f8
Bernardo <beefreguglia@gmail.com>
2025-02-11 11:59:51 -0300

    feature: create sign inonSubimt

--- react/refund/web/src/pages/SignIn.tsx
+++ react/refund/web/src/pages/SignIn.tsx
@@ -1,23 +1,33 @@
+import { useState } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false); 
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    alert('Enviado')
+  }
+
   return (
-    <form className="w-full flex flex-col gap-4">
+    <form className="w-full flex flex-col gap-4" onSubmit={onSubmit}>
       <Input 
         required 
         legend="E-mail" 
         type="email" 
         placeholder="seu@email.com" 
+        onChange={(e) => setEmail(e.target.value)}
       />
       <Input 
         required 
         legend="Senha" 
         type="password"  
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <Button>
-        Entrar
-      </Button>
+      <Button type="submit" isLoading={isLoading}>Entrar</Button>
     </form>
   )
 }
