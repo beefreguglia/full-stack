0f33fa348ac7a25e3251d451c56d4f22b470338a
Bernardo <beefreguglia@gmail.com>
2025-02-11 17:30:43 -0300

    feature: create loading component

--- react/refund/web/src/routes/index.tsx
+++ react/refund/web/src/routes/index.tsx
@@ -1,14 +1,21 @@
 import { BrowserRouter } from "react-router";
-import { ManagerRoutes } from "./ManagerRoutes";
-// import { AuthRoutes } from "./AuthRoutes";
+// import { ManagerRoutes } from "./ManagerRoutes";
+import { AuthRoutes } from "./AuthRoutes";
+import { Loading } from "../components/Loading";
 // import { EmployeeRoutes } from "./EmployeeRoutes";
 
+const isLoading = false
 export function Routes() {
+
+  if(isLoading) {
+    return <Loading />
+  }
+
   return (
     <BrowserRouter>
-      {/* <AuthRoutes /> */}
+      <AuthRoutes />
       {/* <EmployeeRoutes /> */}
-      <ManagerRoutes />
+      {/* <ManagerRoutes /> */}
     </BrowserRouter>
   )
 }
