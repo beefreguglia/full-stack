547bbd71e7974cb19afadb3f7cbcf6982ff06bee
Bernardo <beefreguglia@gmail.com>
2025-03-24 02:05:50 -0300

    feature: create error handling in project

--- node/refund-api/src/app.ts
+++ node/refund-api/src/app.ts
@@ -1,8 +1,13 @@
 import express from "express"
 import cors from "cors"
+import { errorHandling } from "./middlewares/error-handler";
 
 const app = express();
 app.use(cors())
 app.use(express.json())
+
+
+
+app.use(errorHandling)
 
 export { app }
