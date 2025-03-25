ac3ca9c5310029c7f72958845f7fa0e0cbfb98e0
Bernardo <beefreguglia@gmail.com>
2025-03-24 21:21:27 -0300

    feature: create user routes and controller

--- node/refund-api/src/app.ts
+++ node/refund-api/src/app.ts
@@ -1,13 +1,13 @@
 import express from "express"
 import cors from "cors"
 import { errorHandling } from "./middlewares/error-handler";
+import { routes } from "./routes";
 
 const app = express();
 app.use(cors())
 app.use(express.json())
 
-
-
+app.use(routes)
 app.use(errorHandling)
 
 export { app }
