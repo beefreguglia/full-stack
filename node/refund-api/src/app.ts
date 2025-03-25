b2c573d1e4f89a2881bdb3d7375240bfd5233d45
Bernardo <beefreguglia@gmail.com>
2025-03-24 21:40:43 -0300

    feature: finish create user

--- node/refund-api/src/app.ts
+++ node/refund-api/src/app.ts
@@ -1,5 +1,7 @@
+import "express-async-errors"
 import express from "express"
 import cors from "cors"
+
 import { errorHandling } from "./middlewares/error-handler";
 import { routes } from "./routes";
 
