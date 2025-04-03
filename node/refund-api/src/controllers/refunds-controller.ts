874d1d1a9bf0587fbf72e952cef1e5b4477902e6
Bernardo <beefreguglia@gmail.com>
2025-04-03 01:28:31 -0300

    feature: create refund data validation

--- node/refund-api/src/controllers/refunds-controller.ts
+++ node/refund-api/src/controllers/refunds-controller.ts
@@ -1,7 +1,25 @@
 import { Request,  Response } from 'express'
+import { z } from 'zod'
+
+const CategoriesEnum = z.enum([
+  "food", 
+  "others", 
+  "services", 
+  "transport", 
+  "accommodation"
+]);
 
 class RefundsController {
   async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(1, { message: 'Informe o nome da solicitação' }),
+      category: CategoriesEnum,
+      amount: z.number().positive({ message: "O número precisa ser positivo"}),
+      filename: z.string().min(20),
+    })
+
+    const { amount, category, filename, name } = bodySchema.parse(request.body)
+
     response.json({ message: "ok" })
   }
 }
