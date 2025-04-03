f3e2babb5057395e8b72dbd2d631c2e08d236c5a
Bernardo <beefreguglia@gmail.com>
2025-04-03 11:10:31 -0300

    feature: create index refund method and route

--- node/refund-api/src/controllers/refunds-controller.ts
+++ node/refund-api/src/controllers/refunds-controller.ts
@@ -38,6 +38,55 @@ class RefundsController {
 
     response.status(201).json(refund)
   }
+  
+  async index(request: Request, response: Response) {
+    const querySchema =  z.object({
+      name: z.string().trim().optional().default(''),
+      page: z.coerce.number().optional().default(1),
+      perPage: z.coerce.number().optional().default(10),
+    })
+
+    const { name, page, perPage } = querySchema.parse(request.query)
+
+    // Calcula valores do skip.
+    const skip = (page - 1) * perPage;
+
+    const refunds = await prisma.refund.findMany({
+      skip,
+      take: perPage,
+      where: {
+        name: {
+          contains: name,
+        }
+      },
+      include: {
+        user: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    })
+
+    // Obter total de registros para calcular o número de páginas
+    const totalRecords = await prisma.refund.count({
+      where: {
+        name: {
+          contains: name,
+        }
+      },
+    })
+
+    const totalPages = Math.ceil(totalRecords / perPage);
+
+    response.status(200).json({ 
+      refunds,
+      pagination: {
+        page,
+        perPage,
+        totalRecords,
+        totalPages: totalPages > 0 ? totalPages : 1,
+    } })
+  }
 }
 
 export { RefundsController }
